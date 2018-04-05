efa2d6a826e5420405f1e2a4fcd1d912cfc19d6b
cijacob <jacobcindy@live.be>
2018-04-06 00:46:35 +0200

    switch to psql db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180404115753) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "colors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
